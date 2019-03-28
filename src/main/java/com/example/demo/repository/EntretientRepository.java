c15b71d6d4a896e3d79a1a26c2876689c4d6f466
ImedzenneD <imedzenned@yahoo.fr>
2019-03-28 23:52:43 +0100

    adding repositorys

---
+++ src/main/java/com/example/demo/repository/EntretientRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Entretient;
+
+public interface EntretientRepository extends JpaRepository<Entretient, Long> {
+
+}
